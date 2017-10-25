80a6711ad85c3e82997afa10958fda5b74a3ca89
Tom Lord <tom@showmyhomework.co.uk>
2017-10-25 19:26:50 +0100

    V2 - refactoring towards more generic solution

--- razzle_dazzle.rb
+++ razzle_dazzle.rb
@@ -1,3 +1,4 @@
+require 'pry'
 module RazzleDazzle
   class Board
     attr_reader :counts
@@ -23,44 +24,63 @@ module RazzleDazzle
     end
   end
 
-  class Score
-    attr_reader :points
-    def initialize(points: default_points)
-      @points = points
+  class ScoreActionFactory
+    def self.build(added_score: 0)
+      new(added_score)
+    end
+
+    def initialize(added_score)
+      @added_score = added_score
+    end
+
+    def added_score
+      @added_score
+    end
+  end
+
+  class ScoreActions
+    attr_reader :point_actions
+    def initialize(point_actions: default_point_actions)
+      @point_actions = Hash.new(ScoreActionFactory.build).merge(point_actions)
+    end
+
+    def points_for(score)
+      point_actions[score].added_score
     end
 
     private
 
-    def default_points
+    def default_point_actions
       {
-        8 => 10,
-        9 => 8,
-        10 => 5,
-        11 => 5,
-        12 => 5,
-        13 => 5,
-        14 => 1.5,
-        15 => 1.5,
-        16 => 0.5,
-        17 => 0.5,
-        39 => 0.5,
-        40 => 0.5,
-        41 => 1.5,
-        42 => 1.5,
-        43 => 5,
-        44 => 5,
-        45 => 5,
-        46 => 8,
-        47 => 8,
-        48 => 10
+        8 => ScoreActionFactory.build(added_score: 10),
+        9 => ScoreActionFactory.build(added_score: 8),
+        10 => ScoreActionFactory.build(added_score: 5),
+        11 => ScoreActionFactory.build(added_score: 5),
+        12 => ScoreActionFactory.build(added_score: 5),
+        13 => ScoreActionFactory.build(added_score: 5),
+        14 => ScoreActionFactory.build(added_score: 1.5),
+        15 => ScoreActionFactory.build(added_score: 1.5),
+        16 => ScoreActionFactory.build(added_score: 0.5),
+        17 => ScoreActionFactory.build(added_score: 0.5),
+        39 => ScoreActionFactory.build(added_score: 0.5),
+        40 => ScoreActionFactory.build(added_score: 0.5),
+        41 => ScoreActionFactory.build(added_score: 1.5),
+        42 => ScoreActionFactory.build(added_score: 1.5),
+        43 => ScoreActionFactory.build(added_score: 5),
+        44 => ScoreActionFactory.build(added_score: 5),
+        45 => ScoreActionFactory.build(added_score: 5),
+        46 => ScoreActionFactory.build(added_score: 8),
+        47 => ScoreActionFactory.build(added_score: 8),
+        48 => ScoreActionFactory.build(added_score: 10)
       }
     end
   end
 
   class Game
-    def initialize(board: Board.new, score: Score.new)
+    attr_accessor :turns
+    def initialize(board: Board.new, score_actions: ScoreActions.new)
       @board = board
-      @score = score
+      @score_actions = score_actions
       @current_score = 0
       @turns = 0
       @target_score = 10
@@ -68,25 +88,36 @@ module RazzleDazzle
 
     def play
       while @current_score < @target_score do
-        @turns += 1
-        take_turn
+        self.turns += 1
+        this_roll = roll_score
+        @current_score += @score_actions.points_for(this_roll)
       end  
     end
 
-    def take_turn
-      roll_results = roll
-      roll_sum = roll_results.inject(:+)
-      roll_score = @score.points[roll_sum].to_i
-      @current_score += roll_score
-      puts "Turn #{@turns} -- Rolled: #{roll_results}, total: #{roll_sum}, score: #{roll_score} -- total score: #{@current_score}"
-    end
+    private
 
-    def roll
-      @board.possible_scores.sample(8)
+    def roll_score
+      @board.possible_scores.sample(8).inject(:+)
     end
-
   end
+
+  class Simulator
+    def initialize(board: Board.new, score_actions: ScoreActions.new, runs: 1)
+      @board = board
+      @score_actions = score_actions
+      @runs = runs
+    end
+
+    def run
+      (1..@runs).each do |run_number|
+        game = Game.new
+        game.play
+        puts "Simulation ##{run_number}/#{@runs}:"
+        puts "  Total turns: #{game.turns}"
+      end
+    end
+  end
+
 end
 
-# g = RazzleDazzle::Game.new
-# g.play
+RazzleDazzle::Simulator.new(runs: 10).run
